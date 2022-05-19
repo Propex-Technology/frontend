817c4aa0f62323bf11fb715af26c4d407482a2b5
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-05-19 17:00:12 -0500

    Added production level backend.

--- src/contracts.js
+++ src/contracts.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === "development") {
   backendURL = "http://localhost:5001/propex-staging/us-central1/v1"
 }
 else {
-  backendURL = "https://us-central1-propex-staging.cloudfunctions.net/v1";
+  backendURL = "https://us-central1-propex-production.cloudfunctions.net/v1";
 }
 
 export default contracts;
