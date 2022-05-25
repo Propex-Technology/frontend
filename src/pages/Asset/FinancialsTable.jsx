dbd75b28c1df2f6cfe79537ae2e0df6e7c5330a0
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-05-25 17:32:40 -0500

    Added simple financials table

---
+++ src/pages/Asset/FinancialsTable.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import {
+  Button, Table, TableHead, TableRow, TableCell, TableBody, TableContainer,
+} from '@mui/material';
+
+export function FinancialsTable(props) {
+
+  // It's confusing, I know
+  // Sell orders => buy page
+  // Buy orders => sell page
+  const fakeFinancialData = {
+    'Token Breakdown': {
+      'Token Stake Price': '$100',
+      'Also Awesome': '$150'
+    },
+    'Expenditure': {
+      'Token Stake Price': '$100',
+      'Also Awesome': '$150'
+    },
+    'Rental Income': {
+      'Token Stake Price': '$100',
+      'Also Awesome': '$150'
+    }
+  };
+  const tableNames = Object.keys(fakeFinancialData);
+
+  return (
+    tableNames.map((t, i) =>
+      <TableContainer key={i}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>{t}</TableCell>
+              <TableCell align="right"></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Object.entries(fakeFinancialData[t]).map((row, i) => <TableRow
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }} key={i}
+            >
+              <TableCell component="th" scope="row">{row[0]}</TableCell>
+              <TableCell align="right">{row[1]}</TableCell>
+            </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )
+  );
+}
